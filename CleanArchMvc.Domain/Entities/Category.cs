41ddf72175b92e8303c7e3c53a653fd157774ad2
Antônio Vinícius <antoniovrdacosta@gmail.com>
2021-09-16 15:58:58 -0300

    Enriquecendo os domínios

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,11 +1,38 @@
 using System.Collections.Generic;
+using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public class Category : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public string Name { get; private set; }
+        
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+            "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+            "Invalid name. too short, minimum 3 characters");
+
+            Name = name;
+        }
+
         public ICollection<Product> Products { get; set; }
     }
 }
