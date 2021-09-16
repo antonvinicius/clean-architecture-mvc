addc7b117968263b5813310825e74af99b18b72c
Antônio Vinícius <antoniovrdacosta@gmail.com>
2021-09-16 16:05:27 -0300

    Criando interfaces

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+         Task<IEnumerable<Category>> GetCategoriesAsync();
+         Task<Category> GetCategoryAsync(int? id);
+
+         Task<Category> CreateAsync(Category category);
+         Task<Category> UpdateAsync(Category category);
+         Task<Category> RemoveAsync(Category category);
+    }
+}
